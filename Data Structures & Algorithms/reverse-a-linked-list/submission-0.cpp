fafd0b29a0a64de48bfcdb5c5f9ab9383dfd6ca6
nandankamath <75089564+nandankamath@users.noreply.github.com>
2026-07-07 15:49:51 -0400

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            ListNode* next_node = curr->next; 
+            curr->next = prev;              
+            prev = curr;                    
+            curr = next_node;                
+
+        }
+        return prev;
+    }
+};
